3c5bb4309f3984f5ca8001f7cd2eb0081fe1a34a
teoZH <58920759+teoZH@users.noreply.github.com>
2020-11-15 22:16:10 +0200

    Update settings.py

--- media_tests/media_tests/settings.py
+++ media_tests/media_tests/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!897ac*lx3hsymuw!7=7(zz&3wa1gd8$k^kmxbx=+tiohka68j'
+SECRET_KEY = 'somesecretkeyofyourchoice'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
