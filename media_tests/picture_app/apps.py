692fb9a4e0d4e437e04c2091b5cf2fdeb28b0411
teoZH <58920759+teoZH@users.noreply.github.com>
2020-11-16 21:20:00 +0200

    asd
    asd

--- media_tests/picture_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PictureAppConfig(AppConfig):
-    name = 'picture_app'
--- media_tests/picture_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Cars(models.Model):
-    model = models.CharField(blank=False, max_length=20)
-    age = models.PositiveIntegerField(blank=False)
-    price = models.FloatField(blank=False)
-    image = models.ImageField(blank=False, upload_to='cars')
